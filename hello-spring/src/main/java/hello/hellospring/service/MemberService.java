f1e446f461fa97d91f293d2d90156ffce628b0e8
hongchii <flyh0321@gmail.com>
2021-03-27 16:00:03 +0900

    3/27 회원 서비스 테스트 코드

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } //memberRepository를 직접 new로 생성하는게 아니라 외부에서 넣어주게 변경.
 
     /**
      * 회원 가입
---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,71 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach // clear
+    public void afterEach() {
+        memberRepository.clearStore();
+
+    }
+
+    @Test
+    void 회원가입() { // 테스트는 직관적으로 볼 수 있게 한글로 해도 됨.
+        //given 뭔가 주어지고
+        Member member = new Member();
+        member.setName("hello");
+        //when 실행했을때
+        Long saveId = memberService.join(member);
+
+        //then 이렇게 나와야됨
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    // 테스트는 정상 flow도 중요하지만 예외 flow가 훨씬 더 중요하다
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // try catch 보다 간편하게 () -> memberService.join(member2) 이게 실행되면 IllegalStateException 예외가 터
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        /*
+        try {
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+            //isEqualTo 메세지는 MemberService에 validateDuplicateMember메서드 메세지랑 같아야함. 다르면 실패!
+        }
+         */
+    }
+
+    @Test
+    void findOne() {
+    }
+}
