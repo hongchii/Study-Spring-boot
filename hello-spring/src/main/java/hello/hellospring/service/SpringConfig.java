8532974378dc00cffcaabe9fd0cf20419d2e1315
hongchii <flyh0321@gmail.com>
2021-03-29 23:43:06 +0900

    3/29 스프링 DB 접근 기술

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,12 +1,25 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTempleateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+
+        this.dataSource = dataSource;
+    }
 
     @Bean //스프링빈 등록
     public MemberService memberService() {
@@ -16,7 +29,9 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 
-        return new MemoryMemberRepository();
+       // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTempleateMemberRepository(dataSource);
     }
     // MemberService랑 memberRepository 둘 다 스프링빈에 등록
     // MemberService에  private final MemberRepository memberRepository; 에 memberRepository를 넣어줌.
